d80c427850b89e5b891ec0b48aab8ba308588e45
Rashika258 <rashikasuresh5758@gmail.com>
2024-02-11 13:53:18 +0530

    Upload thing configured

--- components/forms/agency-details.tsx
+++ components/forms/agency-details.tsx
@@ -5,28 +5,45 @@ import * as React from "react";
 import { useToast } from "../ui/use-toast";
 import { useRouter } from "next/navigation";
 import { AlertDialog } from "../ui/alert-dialog";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import {zodResolver} from "@hookform/resolvers/zod"
-import { Form } from "../ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { FileUpload } from "../global/file-upload";
+import { Input } from "../ui/input";
+import { Switch } from "../ui/switch";
 
 export interface IAgencyDetailsFormProps {
   data?: Partial<Agency>;
 }
 
 const FormSchema = z.object({
-    name: z.string().min(2, { message: 'Agency name must be atleast 2 chars.' }),
-    companyEmail: z.string().min(1),
-    companyPhone: z.string().min(1),
-    whiteLabel: z.boolean(),
-    address: z.string().min(1),
-    city: z.string().min(1),
-    zipCode: z.string().min(1),
-    state: z.string().min(1),
-    country: z.string().min(1),
-    agencyLogo: z.string().min(1),
-})
+  name: z.string().min(2, { message: "Agency name must be atleast 2 chars." }),
+  companyEmail: z.string().min(1),
+  companyPhone: z.string().min(1),
+  whiteLabel: z.boolean(),
+  address: z.string().min(1),
+  city: z.string().min(1),
+  zipCode: z.string().min(1),
+  state: z.string().min(1),
+  country: z.string().min(1),
+  agencyLogo: z.string().min(1),
+});
 
 export default function AgencyDetails({ data }: IAgencyDetailsFormProps) {
   const { toast } = useToast();
@@ -35,29 +52,29 @@ export default function AgencyDetails({ data }: IAgencyDetailsFormProps) {
   const form = useForm<z.infer<typeof FormSchema>>({
     mode: "onChange",
     resolver: zodResolver(FormSchema),
-    defaultValues:{
-        name: data?.name,
-        companyEmail: data?.companyEmail,
-        companyPhone: data?.companyPhone,
-        whiteLabel: data?.whiteLabel || false,
-        address: data?.address,
-        city: data?.city,
-        zipCode: data?.zipCode,
-        state: data?.state,
-        country: data?.country,
-        agencyLogo: data?.agencyLogo,
+    defaultValues: {
+      name: data?.name,
+      companyEmail: data?.companyEmail,
+      companyPhone: data?.companyPhone,
+      whiteLabel: data?.whiteLabel || false,
+      address: data?.address,
+      city: data?.city,
+      zipCode: data?.zipCode,
+      state: data?.state,
+      country: data?.country,
+      agencyLogo: data?.agencyLogo,
+    },
+  });
+
+  React.useEffect(() => {
+    if (data) {
+      form.reset(data);
     }
-  })
+  }, [data]);
 
-  React.useEffect(()=>{
-    if(data) {
-        form.reset(data)
-    }
-  },[data])
+  const isLoading = form.formState.isSubmitting;
 
-  const handleSubmit = async () =>{
-
-  }
+  const handleSubmit = async () => {};
 
   return (
     <AlertDialog>
@@ -70,9 +87,222 @@ export default function AgencyDetails({ data }: IAgencyDetailsFormProps) {
           </CardDescription>
         </CardHeader>
         <CardContent>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4"></form>
-            </Form>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleSubmit)}
+              className="space-y-4"
+            >
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="agencyLogo"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Agency Logo</FormLabel>
+                    <FormControl>
+                      <FileUpload
+                        apiEndPoint="agencyLogo"
+                        onChange={field.onChange}
+                        value={field.value}
+                      ></FileUpload>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              ></FormField>
+
+              <div className="flex md: flex-row gap-4">
+                <FormField
+                  disabled={isLoading}
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel> Agency Name</FormLabel>
+                      <FormControl>
+                        <Input
+                        placeholder="Yur Agency Name"
+                        {...field}
+                        >
+                        </Input>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+          
+              <div className="flex md:flex-row gap-4">
+                <FormField
+                  disabled={isLoading}
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Agency Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Your agency name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="companyEmail"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Agency Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          readOnly
+                          placeholder="Email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex md:flex-row gap-4">
+                <FormField
+                  disabled={isLoading}
+                  control={form.control}
+                  name="companyPhone"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Agency Phone Number</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Phone"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="whiteLabel"
+                render={({ field }) => {
+                  return (
+                    <FormItem className="flex flex-row items-center justify-between rounded-lg border gap-4 p-4">
+                      <div>
+                        <FormLabel>Whitelabel Agency</FormLabel>
+                        <FormDescription>
+                          Turning on whilelabel mode will show your agency logo
+                          to all sub accounts by default. You can overwrite this
+                          functionality through sub account settings.
+                        </FormDescription>
+                      </div>
+
+                      <FormControl>
+                        <Switch
+                          checked={field.value}
+                          onCheckedChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )
+                }}
+              />
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Address</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="123 st..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex md:flex-row gap-4">
+                <FormField
+                  disabled={isLoading}
+                  control={form.control}
+                  name="city"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>City</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="City"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  disabled={isLoading}
+                  control={form.control}
+                  name="state"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>State</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="State"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  disabled={isLoading}
+                  control={form.control}
+                  name="zipCode"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Zipcpde</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Zipcode"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="country"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Country</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Country"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </AlertDialog>
